893039c43445b9575bcadb2eb0bdcca989620864
Ronar de Jesus <ronardj01@gmail.com>
2021-03-19 17:34:25 +0100

    Completed exercise-1 week-1

--- week-1/InClass/A-objects-intro/exercise-part-0.js
+++ week-1/InClass/A-objects-intro/exercise-part-0.js
@@ -7,7 +7,7 @@ Try to think of as many properties as you can!
 */
 
 let laptop = {
-    brand: "Lenovo",
-    screenSize: 13,
-    isTouchscreen: true,
-  };
+  brand: "Lenovo",
+  screenSize: 13,
+  isTouchscreen: true,
+};
