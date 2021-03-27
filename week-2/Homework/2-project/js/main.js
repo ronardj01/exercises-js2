c15965cc3a9ca6e215ecaebacfe3ae5e2a0245a6
ronardj01 <ronardj01@gmail.com>
2021-03-27 10:19:15 +0100

    Modified part-1 project week-2

--- week-2/Homework/2-project/js/main.js
+++ week-2/Homework/2-project/js/main.js
@@ -6,20 +6,22 @@ const volunteerBtn = document.querySelector('div.jumbotron .btn-secondary')
 //1.2 Variables globales de los botones de cambio de tema.
 const botonAzul = document.querySelector('#blueBtn');
 const botonNaranja = document.querySelector('#orangeBtn');
+const botonGreen = document.querySelector('#greenBtn');
 
+let tema = [
+  { nombre: 'tema azul', jumbotron: '#588fbd', donate: '#ffa500', volunteerBG: 'black', volunteerColor: 'white' },
+  { nombre: 'tema naranja', jumbotron: '#f0ad4e', donate: '#5751fd', volunteerBG: '#31b0d5', volunteerColor: 'white' },
+  { nombre: 'tema verde', jumbotron: '#87ca8a', donate: 'black', volunteerBG: '#8c9c08', volunteerColor: 'black' },
+]
+let temaAzul = ['#588fbd', 'ffa500', 'black', 'white']
 //2. Funcionalidad del boton azul.
-function changingToBlueTheme() {
-  divContent.style.backgroundColor = '#588fbd';
-  donateBtn.style.backgroundColor = '#ffa500';
-  volunteerBtn.style.backgroundColor = 'white';
+function changingTheme(boton, obj) {
+divContent.style.backgroundColor = obj.jumbotron;
+donateBtn.style.backgroundColor = obj.donate;
+volunteerBtn.style.backgroundColor = obj.volunteerBG;
+volunteerBtn.style.color = obj.volunteerColor;
 }
 
-botonAzul.addEventListener('click', changingToBlueTheme);
-
-//3. Funcionalidad del boton naranja.
-function changingToOrangeTheme() {
-  divContent.style.backgroundColor = '#f0ad4e';
-  donateBtn.style.backgroundColor = '#5751fd';
-  volunteerBtn.style.backgroundColor = 'white';
-}
-botonNaranja.addEventListener('click', changingToOrangeTheme);
+botonAzul.addEventListener('click', () => changingTheme(botonAzul, tema[0]));
+botonNaranja.addEventListener('click', () => changingTheme(botonNaranja, tema[1]));
+botonGreen.addEventListener('click', (e) => changingTheme(e.target, tema[2]));
